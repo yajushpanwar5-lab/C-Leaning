56230207039a285c7ea4ac9afbd62553b291726d
Yajush Panwar <yajushpanwar5@gmail.com>
2026-07-22 22:03:57 +0530

    Add files via upload

---
+++ C-Learninng/Chapter_2/chapter2_practiceset.c/ques2.c
@@ -0,0 +1,8 @@
+// What data type will 3.0/8 − 2 return?
+#include <stdio.h>
+
+int main(){
+    float result = 3.0/8 - 2;
+    printf("The result is %f and its data type is float", result);  
+    return 0;
+}
