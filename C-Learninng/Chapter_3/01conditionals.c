3eba04c47e3dd151e19b2fdcb21e45b5efd6d537
Yajush Panwar <yajushpanwar5@gmail.com>
2026-07-22 22:05:13 +0530

    Add files via upload

---
+++ C-Learninng/Chapter_3/01conditionals.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int age = 6;
+    if(age>10){
+        printf("your age is grater than 10!");
+
+    }
+else { printf("your age is less than 10!");
+}    return 0;
+}
